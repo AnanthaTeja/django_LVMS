57bb2b59a9b2d92de9ffefcf50eb810b67a0202c
Anantha Teja Dasari <111896520+AnanthaTeja@users.noreply.github.com>
2023-08-01 00:51:25 +0530

    Add files via upload

---
+++ LApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as ad
+
+urlpatterns = [
+    path("", views.home, name="hm"),
+    path("abt/", views.about, name="ab"),
+    path("cnt/", views.contact, name="ct"),
+    path("reg/", views.register, name="rg"),
+    path("lgn/", ad.LoginView.as_view(template_name="html/login.html"), name="lg"),
+    path("lgot/", ad.LogoutView.as_view(template_name="html/logout.html"), name="lgt"),
+    path("usrlst/", views.userlist, name="usrl"),
+    path("pfle/", views.profile, name="pf"),
+    path("usrup/<int:h>/", views.userupdate, name="usp"),
+    path("usrdel/<int:d>/", views.userdelete, name="usd"),
+    path("upfle/", views.updprofile, name="upf"),
+]
